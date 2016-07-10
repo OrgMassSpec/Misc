6cab7ab23ff2fc74e3d9d22861e1a1b2680e59ed
Nathan Dodder <OrgMassSpec@users.noreply.github.com>
2016-07-10 11:27:09 -0700

    Package install script.

---
+++ Package Install.R
@@ -0,0 +1,21 @@
+# Package install script
+
+install.packages("maps", repos = "http://cran.r-project.org")
+install.packages("mapdata", repos = "http://cran.r-project.org")
+install.packages("maptools", repos = "http://cran.r-project.org")
+install.packages("sp", repos = "http://cran.r-project.org")
+install.packages("RColorBrewer", repos = "http://cran.r-project.org")
+install.packages("latticeExtra", repos = "http://cran.r-project.org")
+install.packages("png", repos = "http://cran.r-project.org")
+install.packages("knitr", repos = "http://cran.r-project.org")
+install.packages("gridExtra", repos = "http://cran.r-project.org")
+install.packages("dplyr", repos = "http://cran.r-project.org")
+install.packages("shiny", repos = "http://cran.r-project.org")
+install.packages("OrgMassSpecR", repos = "http://cran.r-project.org")
+
+# install.packages("xtable", repos = "http://cran.r-project.org")
+#install.packages("randomForest", repos = "http://cran.r-project.org")
+# install.packages("party", repos = "http://cran.r-project.org")
+# install.packages("readxl", repos = "http://cran.r-project.org")
+
+# install.packages("OrgMassSpecR", repos="http://R-Forge.R-project.org")
