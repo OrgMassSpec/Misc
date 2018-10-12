044f07f81a87835af87016ba2564fed1ad0e2017
Nathan Dodder <OrgMassSpec@users.noreply.github.com>
2018-10-11 18:20:44 -0700

    Deleted

--- Package Install.R
+++ Package Install.R
@@ -7,29 +7,27 @@ install.packages("png", repos = "http://cran.r-project.org")
 
 # Tidyverse
 install.packages("tidyverse", repos = "http://cran.r-project.org")
-install.packages("knitr", repos = "http://cran.r-project.org")
 install.packages("rmarkdown", repos = "http://cran.r-project.org")
 install.packages("shiny", repos = "http://cran.r-project.org")
-
-# Maps
-#install.packages("maps", repos = "http://cran.r-project.org")
-#install.packages("mapdata", repos = "http://cran.r-project.org")
-#install.packages("maptools", repos = "http://cran.r-project.org")
-#install.packages("sp", repos = "http://cran.r-project.org")
-install.packages("RColorBrewer", repos = "http://cran.r-project.org")
-
-install.packages("ISLR", repos = "http://cran.r-project.org")
-
-# install.packages("devtools", repos = "http://cran.r-project.org")
-# devtools::install_github("dkahle/ggmap")
-# install.packages("ggmap", repos = "http://cran.r-project.org")
 
 # OrgMassSpecR
 # install.packages("OrgMassSpecR", repos = "http://cran.r-project.org")
 # install.packages("OrgMassSpecR", repos="http://R-Forge.R-project.org")
 
 # Misc
+install.packages("ISLR", repos = "http://cran.r-project.org")
+install.packages("data.table", repos = "http://cran.r-project.org")
 # install.packages("xtable", repos = "http://cran.r-project.org")
 # install.packages("randomForest", repos = "http://cran.r-project.org")
 # install.packages("party", repos = "http://cran.r-project.org")
 # install.packages("readxl", repos = "http://cran.r-project.org")
+# install.packages("devtools", repos = "http://cran.r-project.org")
+# devtools::install_github("dkahle/ggmap")
+# install.packages("ggmap", repos = "http://cran.r-project.org")
+
+# Maps
+#install.packages("maps", repos = "http://cran.r-project.org")
+#install.packages("mapdata", repos = "http://cran.r-project.org")
+#install.packages("maptools", repos = "http://cran.r-project.org")
+#install.packages("sp", repos = "http://cran.r-project.org")
+#install.packages("RColorBrewer", repos = "http://cran.r-project.org")
