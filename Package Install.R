e80e21be55324556d0c89004e73f94b88b3d179f
Nathan Dodder <OrgMassSpec@users.noreply.github.com>
2020-04-27 13:31:27 -0700

    Update Package Install.R

--- Package Install.R
+++ Package Install.R
@@ -1,17 +1,18 @@
 # Package install script
 
+# Tidyverse
+install.packages("tidyverse", repos = "http://cran.r-project.org")
+# install.packages("shiny", repos = "http://cran.r-project.org")
+# install.packages("kableExtra", repos = "http://cran.r-project.org")
+# install.packages("rmarkdown", repos = "http://cran.r-project.org")
+
 # Plotting
 install.packages("latticeExtra", repos = "http://cran.r-project.org")
 install.packages("png", repos = "http://cran.r-project.org")
 # install.packages("gridExtra", repos = "http://cran.r-project.org")
 
-# Tidyverse
-install.packages("tidyverse", repos = "http://cran.r-project.org")
-install.packages("rmarkdown", repos = "http://cran.r-project.org")
-install.packages("shiny", repos = "http://cran.r-project.org")
-
 # OrgMassSpecR
-# install.packages("OrgMassSpecR", repos = "http://cran.r-project.org")
+install.packages("OrgMassSpecR", repos = "http://cran.r-project.org")
 # install.packages("OrgMassSpecR", repos="http://R-Forge.R-project.org")
 
 # Misc
@@ -20,14 +21,14 @@ install.packages("data.table", repos = "http://cran.r-project.org")
 # install.packages("xtable", repos = "http://cran.r-project.org")
 # install.packages("randomForest", repos = "http://cran.r-project.org")
 # install.packages("party", repos = "http://cran.r-project.org")
-# install.packages("readxl", repos = "http://cran.r-project.org")
 # install.packages("devtools", repos = "http://cran.r-project.org")
-# devtools::install_github("dkahle/ggmap")
-# install.packages("ggmap", repos = "http://cran.r-project.org")
+# install.packages("readxl", repos = "http://cran.r-project.org")
 
 # Maps
-#install.packages("maps", repos = "http://cran.r-project.org")
-#install.packages("mapdata", repos = "http://cran.r-project.org")
-#install.packages("maptools", repos = "http://cran.r-project.org")
-#install.packages("sp", repos = "http://cran.r-project.org")
-#install.packages("RColorBrewer", repos = "http://cran.r-project.org")
+# install.packages("maps", repos = "http://cran.r-project.org")
+# install.packages("mapdata", repos = "http://cran.r-project.org")
+# install.packages("maptools", repos = "http://cran.r-project.org")
+# install.packages("sp", repos = "http://cran.r-project.org")
+# install.packages("RColorBrewer", repos = "http://cran.r-project.org")
+# install.packages("ggmap", repos = "http://cran.r-project.org")
+# devtools::install_github("dkahle/ggmap")
